1a3cb83486a23b8d9b7a521f0512029c573a0d7c
cjh <mordiumaco@gamil.com>
2018-11-30 12:46:09 +0900

    메인 컨트롤러 복구

---
+++ src/main/java/kr/co/tripweaver/main/web/MainController.java
@@ -0,0 +1,92 @@
+package kr.co.tripweaver.main.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/main")
+@Controller
+public class MainController {
+
+
+	@RequestMapping("/main")
+	public String mainView() {
+		return "index";
+	}
+
+	@RequestMapping("/login")
+	public String loginView() {
+		return "login/login";
+	}
+
+	@RequestMapping("/register")
+	public String registerView() {
+		return "login/register";
+	}
+
+	@RequestMapping("/register_form")
+	public String register_formView() {
+		return "login/register_form";
+	}
+
+	@RequestMapping("/idPw_lost")
+	public String idPw_lostView() {
+		return "login/idPw_lost";
+	}
+	
+	@RequestMapping("/adminMain")
+	public String adminMainView(Model model) {
+		model.addAttribute("gnb", 0);
+		return "admin/adminMain";
+	}
+	
+	@RequestMapping("/member")
+	public String memberView(Model model) {
+		model.addAttribute("gnb", 1);
+		return "admin/membership/member";
+	}
+	
+	@RequestMapping("/create_board")
+	public String create_boardView(Model model) {
+		model.addAttribute("gnb", 2);
+		return "admin/board/create_board";
+	}
+	
+	@RequestMapping("/declaration")
+	public String declarationView(Model model) {
+		model.addAttribute("gnb", 0);
+		return "admin/setting/declaration";
+	}
+	
+	@RequestMapping("/contact")
+	public String contactView(Model model) {
+		model.addAttribute("gnb", 3);
+		return "admin/statistics/contact";
+	}
+	
+	@RequestMapping("/contactTime")
+	public String contactTimeView(Model model) {
+		model.addAttribute("gnb", 3);
+		return "admin/statistics/contactTime";
+	}
+	
+	@RequestMapping("/hashTag")
+	public String hashTagView(Model model) {
+		model.addAttribute("gnb", 3);
+		return "admin/statistics/hashTag";
+	}
+	
+	@RequestMapping("/adminPost")
+	public String adminPostView(Model model) {
+		model.addAttribute("gnb", 3);
+		return "admin/statistics/post";
+	}
+	
+	@RequestMapping("/codetable")
+	public String codetableView(Model model) {
+		model.addAttribute("gnb", 4);
+		return "admin/codetable/codetable";
+	}
+	
+
+}
