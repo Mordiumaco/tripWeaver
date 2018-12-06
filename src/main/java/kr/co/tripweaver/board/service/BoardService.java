1de7a088604df3ac0f2a461bd7b3803a7fd25b24
dongjoo <choero2767@gmail.com>
2018-12-06 16:59:15 +0900

    게시판 생성자 받기, 리스트 받아오기

--- src/main/java/kr/co/tripweaver/board/service/BoardService.java
+++ src/main/java/kr/co/tripweaver/board/service/BoardService.java
@@ -1,15 +1,31 @@
 package kr.co.tripweaver.board.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import kr.co.tripweaver.board.dao.IBoardDao;
 import kr.co.tripweaver.board.model.BoardVO;
 
+@Service
 public class BoardService implements IBoardService{
 
 	@Autowired
 	IBoardDao boardDao;
 
+	/**
+	* Method : selectBoardAllList
+	* 작성자 : pc23
+	* 변경이력 :
+	* @return
+	* Method 설명 : 게시판 전체 불러오기
+	*/
+	@Override
+	public List<BoardVO> selectBoardAllList() {
+		return boardDao.selectBoardAllList();
+	}
+	
 	/**
 	* Method : insertBoard
 	* 작성자 : pc23
@@ -21,5 +37,6 @@ public class BoardService implements IBoardService{
 	public int insertBoard(BoardVO boardVo) {
 		return boardDao.insertBoard(boardVo);
 	}
+
 	
 }
--- src/main/java/kr/co/tripweaver/board/web/BoardController.java
+++ src/main/java/kr/co/tripweaver/board/web/BoardController.java
@@ -1,9 +1,12 @@
 package kr.co.tripweaver.board.web; 
 
+import java.util.List;
+
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,16 +22,30 @@ public class BoardController {
 	@Resource(name="boardService")
 	private IBoardService boardService;
 	
+	@RequestMapping(value="/boardCreate", method=RequestMethod.GET)
+	public String boardCreateView(@RequestParam("mem_id")String mem_id, Model model) {
+		
+		List<BoardVO> boardList = boardService.selectBoardAllList();
+		
+		model.addAttribute("boardList", boardList);
+		
+		return "admin/board/create_board";
+	}
+	
 	@RequestMapping(value="/boardCreate", method=RequestMethod.POST)
 	public String boardCreate(HttpServletRequest request, @RequestParam("board_title")String board_title, @RequestParam("board_use")String board_use) {
 		
 		MemberVO memberVo = (MemberVO)request.getAttribute("loginInfo");
 		
+		String mem_id = memberVo.getMem_id();
+		
 		BoardVO boardVo = new BoardVO();
 		boardVo.setBoard_title(board_title);
 		boardVo.setBoard_use(board_use);
 		
-		return null;
+		int insertCnt = boardService.insertBoard(boardVo);
+		
+		return "admin/board/create_board";
 	}
 
 }
