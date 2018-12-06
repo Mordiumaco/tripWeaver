c3b7a1115fff3848681f840bb17cee626d3531a4
cjh <mordiumaco@gamil.com>
2018-12-06 16:36:50 +0900

    login Session 실수 정리

--- src/main/java/kr/co/tripweaver/login/service/LoginService.java
+++ src/main/java/kr/co/tripweaver/login/service/LoginService.java
@@ -4,7 +4,6 @@ import java.io.UnsupportedEncodingException;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.websocket.Session;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
