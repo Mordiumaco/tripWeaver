7534a8e6a2ede8419ce0f31267e2a6e1f479a756
jin <parkjinq@gmail.com>
2018-12-11 09:11:53 +0900

    1.팔로우/팔로잉 구현 완료 2.채팅방 조회 입장 3.채팅방 메세지 조회

--- src/main/java/kr/co/tripweaver/mymenu/mypage/follow_following/web/FollowController.java
+++ src/main/java/kr/co/tripweaver/mymenu/mypage/follow_following/web/FollowController.java
@@ -1,8 +1,45 @@
 package kr.co.tripweaver.mymenu.mypage.follow_following.web;
 
-import org.springframework.stereotype.Controller;
+import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import kr.co.tripweaver.mymenu.mypage.follow_following.model.FollowVO;
+import kr.co.tripweaver.mymenu.mypage.follow_following.service.IFollowService;
+
+@RequestMapping("/follow")
 @Controller
 public class FollowController {
-
+	
+	@Autowired
+	private IFollowService followService;
+	
+	private Logger logger = LoggerFactory.getLogger(FollowController.class);
+	
+	@RequestMapping(value="/following", method= RequestMethod.POST)
+	public void following(FollowVO followVO) {
+		logger.debug("following followVO : {}", followVO);
+		int cnt = followService.insertFollowing(followVO);
+	}
+	
+	@RequestMapping(value="/unfollow", method= RequestMethod.POST)
+	public void unfollow(FollowVO followVO) {
+		logger.debug("unfollow followVO : {}", followVO);
+		int cnt = followService.deleteFollowing(followVO);
+	}
+	
+	@RequestMapping(value="/followCountAjax", method= {RequestMethod.POST, RequestMethod.GET})
+	public String followCount(@RequestParam("mem_id") String mem_id, Model model) {
+		Map<String, Object> resultMap = followService.followCount(mem_id);
+		model.addAllAttributes(resultMap);
+		return "/mypage/message/message_follow_count_ajax";
+	}
+	
 }
--- src/main/java/kr/co/tripweaver/mymenu/mypage/message/service/IMessageService.java
+++ src/main/java/kr/co/tripweaver/mymenu/mypage/message/service/IMessageService.java
@@ -1,6 +1,9 @@
 package kr.co.tripweaver.mymenu.mypage.message.service;
 
+import java.util.List;
 import java.util.Map;
+
+import kr.co.tripweaver.mymenu.mypage.message.model.MessageVO;
 
 public interface IMessageService {
 
@@ -12,5 +15,25 @@ public interface IMessageService {
 	* @return
 	* Method 설명 : 해당 멤버의 message.jsp 필요한 팔로잉/팔로워 정보(인원수,멤버리스트)를 반환하는 메서드
 	 */
-	public Map<String, Object> messagePageView(String mem_id);
+	public Map<String, Object> messagePageView(Map<String, Object> params);
+	
+	/**
+	* Method : selectChatroom
+	* 작성자 : jin
+	* 변경이력 :
+	* @param mem_id
+	* @return
+	* Method 설명 : 로그인유저의 채팅방 목록을 반환하는 메서드
+	 */
+	public List<MessageVO> selectChatroom(String mem_id);
+	
+	/**
+	* Method : enterChatroom
+	* 작성자 : jin
+	* 변경이력 :
+	* @param params
+	* @return
+	* Method 설명 : (채팅방 입장)선택한 채팅방에 대한 정보를 반환하는 메서드
+	 */
+	public Map<String, Object> enterChatroom(Map<String, Object> params);
 }
