d1a58d6c8a5f22def39718cc5e7cb0931f0eb7e8
Mordium <mordiumaco@gmail.com>
2018-12-24 21:49:47 +0900

    1. plan section id refactoring 2. like table section processing working

--- src/main/java/kr/co/tripweaver/mymenu/reservation/dao/ReservationDao.java
+++ src/main/java/kr/co/tripweaver/mymenu/reservation/dao/ReservationDao.java
@@ -1,5 +1,7 @@
 package kr.co.tripweaver.mymenu.reservation.dao;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 
 import org.mybatis.spring.SqlSessionTemplate;
@@ -35,9 +37,60 @@ public class ReservationDao implements IReservationDao {
 			return result;
 		}
 		
-		
 		return result;
+	}
+
+	
+	/**
+	* Method : selectReserByMemId
+	* 작성자 : Jae Hyeon Choi
+	* 생성날짜 : 2018. 12. 24.
+	* 변경이력 :
+	* @param mem_id
+	* @return
+	* Method 설명 : 로그인한 회원의 아이디로 해당 회원의 예약정보를 받아온다. 
+	*/
+	@Override
+	public List<ReservationVO> selectReserByMemId(String mem_id) {
+		
+		List<ReservationVO> reservationVo = null;
+		
+		try {
+			
+			reservationVo = template.selectList("reservationSQL.selectReserByMemId", mem_id);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return reservationVo;
+		}
+		
+		return reservationVo;
 	}
 	
 	
+	/**
+	* Method : reserTotalPeopleCountByGuidePlanId
+	* 작성자 : Jae Hyeon Choi
+	* 생성날짜 : 2018. 12. 24.
+	* 변경이력 :
+	* @return
+	* Method 설명 : 해당 가이드 예약일자에 대한 총 예약 인원 수
+	*/
+	public Integer reserTotalPeopleCountByGuidePlanId(String guideplan_id) {
+		
+		Integer total_people_count = null;
+		
+		try {
+			
+			total_people_count = template.selectOne("reservationSQL.reserTotalPeopleCountByGuidePlanId", guideplan_id);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return total_people_count;
+		}
+		
+		return total_people_count;
+		
+		
+	}
 }
--- src/main/java/kr/co/tripweaver/mymenu/reservation/service/ReservationService.java
+++ src/main/java/kr/co/tripweaver/mymenu/reservation/service/ReservationService.java
@@ -1,5 +1,7 @@
 package kr.co.tripweaver.mymenu.reservation.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,8 +22,42 @@ public class ReservationService implements IReservationService{
 	 * @param reservationVo
 	 * @return
 	 */
+	@Override
 	public int insertReser(ReservationVO reservationVo){
-		return reservationDao.insertReser(reservationVo);
+		
+		int result =  reservationDao.insertReser(reservationVo);
+		
+		return result;
+	}
+	
+	
+	/**
+	* Method : selectReserByMemId
+	* 작성자 : Jae Hyeon Choi
+	* 생성날짜 : 2018. 12. 24.
+	* 변경이력 :
+	* @param mem_id
+	* @return
+	* Method 설명 : 로그인한 회원의 아이디로 해당 회원의 예약정보를 받아온다. 
+	*/
+	@Override
+	public List<ReservationVO> selectReserByMemId(String mem_id){
+		return reservationDao.selectReserByMemId(mem_id);
+	}
+	
+	
+	
+	/**
+	* Method : reserTotalPeopleCountByGuidePlanId
+	* 작성자 : Jae Hyeon Choi
+	* 생성날짜 : 2018. 12. 24.
+	* 변경이력 :
+	* @return
+	* Method 설명 : 해당 가이드 예약일자에 대한 총 예약 인원 수
+	*/
+	@Override
+	public Integer reserTotalPeopleCountByGuidePlanId(String guideplan_id) {
+		return reservationDao.reserTotalPeopleCountByGuidePlanId(guideplan_id);
 	}
 	
 	
